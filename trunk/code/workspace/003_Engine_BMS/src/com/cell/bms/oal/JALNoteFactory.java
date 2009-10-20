3dad5f203ead87f786d704ad4a61a6c7b1f72135
wazazhang <wazazhang@9c99321c-9dee-11de-8e8d-3741ea1c8a87>
2009-10-20 06:10:52 +0000

    1. 成功突破openal 30个source限制

--- trunk/code/workspace/003_Engine_BMS/src/com/cell/bms/oal/JALNoteFactory.java
+++ trunk/code/workspace/003_Engine_BMS/src/com/cell/bms/oal/JALNoteFactory.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import net.java.games.joal.AL;
+import net.java.games.joal.ALC;
+import net.java.games.joal.ALCcontext;
+import net.java.games.joal.ALCdevice;
+import net.java.games.joal.ALConstants;
 import net.java.games.joal.ALException;
 import net.java.games.joal.ALFactory;
 import net.java.games.joal.util.ALut;
 import net.java.games.sound3d.AudioSystem3D;
 
+import com.cell.CUtil;
 import com.cell.bms.BMSFile;
 import com.cell.bms.IDefineImage;
 import com.cell.bms.IDefineSound;
@@ -17,7 +22,7 @@ import com.cell.bms.JAwtImage;
 public class JALNoteFactory extends com.cell.bms.NoteFactory
 {  
 	AL	al;
-
+	ALC alc;
 	// Position of the listener.
 	private float[] listenerPos = { 0.0f, 0.0f, 0.0f };
 	// Velocity of the listener.
@@ -25,7 +30,7 @@ public class JALNoteFactory extends com.cell.bms.NoteFactory
 	// Orientation of the listener. (first 3 elems are "at", second 3 are "up")
 	private float[] listenerOri = { 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f };
 
-	ArrayList<JALPlayer>	players = new ArrayList<JALPlayer>();
+	ArrayList<JALPlayer>	players = new ArrayList<JALPlayer>(255);
 	
 	
 	public JALNoteFactory() throws Exception
@@ -34,13 +39,50 @@ public class JALNoteFactory extends com.cell.bms.NoteFactory
 		ALut.alutInit();
 		al = ALFactory.getAL();
 		al.alGetError();
+		alc = ALFactory.getALC();
+		
+		// set device
+		{
+			String[] devices = alc.alcGetDeviceSpecifiers();
+			
+			int max_source = 0;
+			int max_index = 0;
+			
+			for (int i=0; i<devices.length; i++) 
+			{
+				System.out.println("Device : " + devices[i]);
+				
+				ALCdevice device = alc.alcOpenDevice(devices[i]);
+				
+				int[] nummono	= new int[1];
+				int[] numstereo = new int[1];
+				alc.alcGetIntegerv(device, ALC.ALC_MONO_SOURCES, 1, nummono, 0); 
+				alc.alcGetIntegerv(device, ALC.ALC_STEREO_SOURCES, 1, numstereo, 0); 
 
+				System.out.println("\tMax mono sources   : " + nummono[0]); 
+				System.out.println("\tMax stereo sources : " + numstereo[0]); 
+				
+				if (max_source < (nummono[0] + numstereo[0])) {
+					max_source = nummono[0] + numstereo[0];
+					max_index = i;
+				}
+			}
+
+			ALCdevice soft_device = alc.alcOpenDevice(devices[max_index]);
+			ALCcontext context = alc.alcCreateContext(soft_device, null);
+			alc.alcMakeContextCurrent(context);
+			
+		}
+		
 		// set listeners
-	    al.alListenerfv(AL.AL_POSITION, listenerPos, 0);
-	    al.alListenerfv(AL.AL_VELOCITY, listenerVel, 0);
-	    al.alListenerfv(AL.AL_ORIENTATION, listenerOri, 0);
+	    {
+	    	al.alListenerfv(AL.AL_POSITION, listenerPos, 0);
+		    al.alListenerfv(AL.AL_VELOCITY, listenerVel, 0);
+		    al.alListenerfv(AL.AL_ORIENTATION, listenerOri, 0);
+	    }
 	    
-		for (int i=0; i<128; i++) {
+	    // create players
+		for (int i=0; i<255; i++) {
 	  		try{
 	  			players.add(new JALPlayer(al));
 	  		}catch(Exception err){
@@ -87,4 +129,6 @@ public class JALNoteFactory extends com.cell.bms.NoteFactory
 		}
 		return null;
 	}
+	
+	
 }
