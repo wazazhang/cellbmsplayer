d7f511637761b8f141877e01a38527758de7d673
wazazhang <wazazhang@9c99321c-9dee-11de-8e8d-3741ea1c8a87>
2010-04-28 05:46:01 +0000

    分离核心引擎和图形

---
+++ code/workspace/000_Engine_Core/gfx/com/cell/gfx/gui/ItemListener.java
@@ -0,0 +1,11 @@
+package com.cell.gfx.gui;
+
+import com.cell.gfx.gui.Command;
+import com.cell.gfx.gui.Item;
+
+public interface ItemListener {
+	
+	public int itemCommandAction(Command command, Item item);
+	
+	
+}
