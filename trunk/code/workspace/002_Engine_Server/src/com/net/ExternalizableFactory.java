e93a6fdd70507f7fa95c11c4974cd54fbc623636
wazazhang <wazazhang@9c99321c-9dee-11de-8e8d-3741ea1c8a87>
2011-01-21 03:40:37 +0000

    1. 简化手工序列化消息的注册

--- trunk/code/workspace/002_Engine_Server/src/com/net/ExternalizableFactory.java
+++ trunk/code/workspace/002_Engine_Server/src/com/net/ExternalizableFactory.java
@@ -1,57 +1,76 @@
 package com.net;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
-
-import com.cell.exception.NotImplementedException;
-import com.net.anno.ExternalizableMessageType;
-import com.sun.xml.internal.txw2.IllegalAnnotationException;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * 类型和 integer 的映射关系，用于 TransmissionType = TRANSMISSION_TYPE_EXTERNALIZABLE 类型的消息。
  */
-public abstract class ExternalizableFactory 
+public abstract class ExternalizableFactory implements Comparator<Class<?>>
 {
-	final Map<Integer, Class<?>> type_map;
+	final private TreeSet<Class<?>>			all_types		= new TreeSet<Class<?>>(this);
+	final private Map<Integer, Class<?>>	map_id_type		= new HashMap<Integer, Class<?>>();
+	final private Map<Class<?>, Integer>	map_type_id		= new HashMap<Class<?>, Integer>();
 	
-	public ExternalizableFactory() {
-		this.type_map = createMap();
+	public ExternalizableFactory() {}	
+	
+	@Override
+	public int compare(Class<?> o1, Class<?> o2) {
+		return o1.getCanonicalName().compareTo(o2.getCanonicalName());
 	}
-
-	/**
-	 * 子类提供的表实现
-	 * @return
-	 */
-	abstract protected Map<Integer, Class<?>> createMap();
 	
 	public int getType(MessageHeader message) {
-		ExternalizableMessageType ext_type = message.getClass().getAnnotation(ExternalizableMessageType.class);
-		return ext_type.value();
+		return map_type_id.get(message.getClass());
 	}
 	
 	public MessageHeader createMessage(int type) throws InstantiationException, IllegalAccessException   {
-		Class<?> ext_type = type_map.get(type);
+		Class<?> ext_type = map_id_type.get(type);
 		return (MessageHeader)ext_type.newInstance();
 	}
 	
+	public Map<Integer, Class<?>> getRegistTypes() {
+		return new TreeMap<Integer, Class<?>>(map_id_type);
+	}
+	
 	/**
-	 * 将标注{@link ExternalizableMessageType}的类注册到系统
-	 * @param clazz
+	 * 注册所有的类后，记得调用此句
 	 */
-	public void registClass(Class<?> clazz) throws Exception {
-		clazz.asSubclass(ExternalizableMessage.class);
-		ExternalizableMessageType ext_type = clazz.getAnnotation(ExternalizableMessageType.class);
-		if (ext_type == null) {
-			throw new NotImplementedException("not Annotation ExternalizableMessageType : " + clazz);
-		} else {
-			Class<?> sc = type_map.get(ext_type.value());
-			if (sc == null) {
-				type_map.put(ext_type.value(), clazz);
-			} else {
-				throw new IllegalAnnotationException("duplicate Annotation ExternalizableMessageType : " +
-						"\"" + clazz + "\"(" + ext_type.value() + ") src=\""+sc.getName()+"\"");
+	protected Map<Integer, Class<?>> syncAll() {
+		synchronized (all_types) {
+			int index = 1;
+			map_id_type.clear();
+			map_type_id.clear();
+			for (Class<?> cls : all_types) {
+				map_id_type.put(index, cls);
+				map_type_id.put(cls, index);
+				index ++;
 			}
 		}
+		return getRegistTypes();
 	}
+	
+//	/**
+//	 * 将标注{@link ExternalizableMessageType}的类注册到系统
+//	 * @param clazz
+//	 */
+//	public void registClass(Class<?> clazz) throws Exception {
+//		clazz.asSubclass(ExternalizableMessage.class);
+//		ExternalizableMessageType ext_type = clazz.getAnnotation(ExternalizableMessageType.class);
+//		if (ext_type == null) {
+//			throw new NotImplementedException("not Annotation ExternalizableMessageType : " + clazz);
+//		} else {
+//			Class<?> sc = type_map.get(ext_type.value());
+//			if (sc == null) {
+//				type_map.put(ext_type.value(), clazz);
+//			} else {
+//				throw new IllegalAnnotationException("duplicate Annotation ExternalizableMessageType : " +
+//						"\"" + clazz + "\"(" + ext_type.value() + ") src=\""+sc.getName()+"\"");
+//			}
+//		}
+//	}
 	
 	
 	/**
@@ -59,16 +78,23 @@ public abstract class ExternalizableFactory
 	 * @param cls
 	 */
 	public void registClasses(Class<?> cls) {
-		try {
-			cls.asSubclass(ExternalizableMessage.class);
-			ExternalizableMessageType ext_type = cls.getAnnotation(ExternalizableMessageType.class);
-			if (ext_type != null) {
-				registClass(cls);
+		synchronized (all_types) {
+			if (ExternalizableMessage.class.isAssignableFrom(cls)) {
+				all_types.add(cls);
 			}
-		} catch (Exception err) {}
-		
-		for (Class<?> sub : cls.getClasses()) {
-			registClasses(sub);
+			for (Class<?> sub : cls.getClasses()) {
+				registClasses(sub);
+			}
 		}
+//		try {
+//			cls.asSubclass(ExternalizableMessage.class);
+//			ExternalizableMessageType ext_type = cls.getAnnotation(ExternalizableMessageType.class);
+//			if (ext_type != null) {
+//				registClass(cls);
+//			}
+//		} catch (Exception err) {}
+//		for (Class<?> sub : cls.getClasses()) {
+//			registClasses(sub);
+//		}
 	}
 }
--- trunk/code/workspace/002_Engine_Server/src/com/net/anno/ExternalizableMessageType.java
+++ trunk/code/workspace/002_Engine_Server/src/com/net/anno/ExternalizableMessageType.java
@@ -14,6 +14,7 @@ import java.lang.annotation.Target;
 @Documented
 @Target({TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Deprecated
 public @interface ExternalizableMessageType 
 {
 	public int value();
