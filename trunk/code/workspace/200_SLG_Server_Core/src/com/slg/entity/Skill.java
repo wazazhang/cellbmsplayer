86367906a3290c278422c47f69f8ef4c30185846
yagami0079@gmail.com <yagami0079@gmail.com@9c99321c-9dee-11de-8e8d-3741ea1c8a87>
2011-06-01 03:18:05 +0000

    道具，技能初步数据

---
+++ trunk/code/workspace/200_SLG_Server_Core/src/com/slg/entity/Skill.java
@@ -0,0 +1,23 @@
+package com.slg.entity;
+
+/**
+ * 技能
+ * @author yagami0079
+ *
+ */
+public class Skill {
+	/**
+	 * 等级
+	 */
+	public int level;
+	/**
+	 * 技能名
+	 */
+	public String name;
+	/**
+	 * 技能描述
+	 */
+	public String desc;
+	
+	
+}
