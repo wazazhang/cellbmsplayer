bc3539756ddface8c97c1286dd3663a1f76da9c7
yagami0079@gmail.com <yagami0079@gmail.com@9c99321c-9dee-11de-8e8d-3741ea1c8a87>
2011-06-07 03:14:13 +0000

    实现层搬到core工程

--- code/workspace/200_SLG_Server_Core/src/com/slg/IWorld.java
+++ code/workspace/200_SLG_Server_Core/src/com/slg/IWorld.java
@@ -3,5 +3,7 @@ package com.slg;
 public interface IWorld {
 	public IPlayer getPlayer(int uid);
 	
+	public IPlayer getPlayer(String name);
+	
 	public int addPlayer(IPlayer newplayer);
 }
--- code/workspace/200_SLG_Server_Core/src/com/slg/net/impl/WorldImpl.java
+++ code/workspace/200_SLG_Server_Core/src/com/slg/net/impl/WorldImpl.java
@@ -7,12 +7,15 @@ import com.slg.IPlayer;
 import com.slg.IWorld;
 
 public class WorldImpl implements IWorld {
+	static private AtomicInteger player_ids = new AtomicInteger(0);
 	//所有玩家
 	private ConcurrentHashMap<Integer, IPlayer> playerimpl_map;
-	static private AtomicInteger player_ids = new AtomicInteger(0);
+	// 名字——>ID表
+	private ConcurrentHashMap<String, Integer> id_name_map;
 
 	public WorldImpl(){
 		playerimpl_map = new ConcurrentHashMap<Integer, IPlayer>();
+		id_name_map = new ConcurrentHashMap<String, Integer>();
 	}
 	@Override
 	public IPlayer getPlayer(int uid) {
@@ -21,7 +24,16 @@ public class WorldImpl implements IWorld {
 	@Override
 	public int addPlayer(IPlayer newplayer) {
 		newplayer.setPlayerID(player_ids.incrementAndGet());
+		playerimpl_map.put(newplayer.getPlayerID(), newplayer);
+		id_name_map.put(newplayer.getPlayerName(), newplayer.getPlayerID());
 		return 0;
+	}
+	@Override
+	public IPlayer getPlayer(String name) {
+		if (id_name_map.get(name)==null){
+			return null;
+		}
+		return playerimpl_map.get(id_name_map.get(name));
 	}
 
 }
